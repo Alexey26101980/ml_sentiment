898adc4f48f3bf40c7f2984fb68859090f6ee5e9
Alexey26101980 <Alekseipodlyagin@mail.ru>
2022-01-15 14:07:00 +0300

    Added sentiment.py and requirements.txt

--- sentiment.py
+++ sentiment.py
@@ -1,3 +1,18 @@
+from fastapi import FastAPI
 from transformers import pipeline
-classifier = pipeline("sentiment-analysis", "blanchefort/rubert-base-cased-sentiment")
-classifier("Я обожаю инженерию машинного обучения!")
+from pydantic import BaseModel
+
+app = FastAPI()
+classifier = pipeline("sentiment-analysis")
+
+class Item(BaseModel):
+    text: str
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.post("/predict/")
+def predict(item:Item):
+    """Sentiment-analysis for text"""
+    return classifier(item.text)[0]
